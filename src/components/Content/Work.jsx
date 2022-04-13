0c6ce0762c8a430e4ce64f3ef219bdc2972761be
hamdidev <itechtranss77@gmail.com>
2022-04-12 21:18:52 -0800

    working on projects section

--- src/components/Content/Work.jsx
+++ src/components/Content/Work.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import {
-  WorkContainer,
-  WorkImg,
+  LinksContainer,
+  ExternalLink,
   WorkCard,
-  WorkImgWrapper,
-  WorkInfo,
-  WorkInfo_Desc,
-  WorkInfo_Title,
-  LinksWrapper,
+  WorkGroup,
+  WorkImage,
+  WorkModal,
+  WorkSubtitle,
+  WorkTitle,
+  GitHubLink,
 } from "./WorkStyles";
 import "./Work.scss";
 import { Container } from "../../globalStyle";
@@ -23,190 +24,43 @@ const Work = () => {
         <ClientTitle>Recent Works</ClientTitle>
         <ClientText>I’ve been working with these technologies.</ClientText>
       </ClientTextWrapper>
-      <WorkContainer>
-        <WorkCard>
-          <WorkImgWrapper>
-            <WorkImg src="/images/work/work1.jpg" />
-            <WorkInfo>
-              <WorkInfo_Title>Web Dev Design</WorkInfo_Title>
-              <WorkInfo_Desc>
-                {" "}
-                I enjoy creating things that live on the internet, whether that
-                be websites, applications, or anything in between.
-              </WorkInfo_Desc>
-              <LinksWrapper>
-                <a href="">
+      <WorkCard>
+        <WorkGroup>
+          {/* image */}
+          <WorkImage src="/images/work/work1.jpg" />
+          <WorkModal>
+            <div>
+              <WorkSubtitle>Web</WorkSubtitle>
+              <WorkTitle>Payment Site</WorkTitle>
+              <LinksContainer>
+                <GitHubLink>
                   <img
                     className="svg-img"
                     src="/images/github.svg"
                     alt="Github Icon"
+                    fill="red"
+                    width="80px"
+                    height="80px"
                   />
-                </a>
-                <a href="">
-                  <img
-                    className="svg-img"
-                    src="/images/external-link.svg"
-                    alt="External Link Icon"
-                  />
-                </a>
-              </LinksWrapper>
-            </WorkInfo>
-          </WorkImgWrapper>
-        </WorkCard>
-        <WorkCard>
-          <WorkImgWrapper>
-            <WorkImg src="/images/work/work2.jpg" />
-            <WorkInfo>
-              <WorkInfo_Title>Web Dev Design</WorkInfo_Title>
-
-              <WorkInfo_Desc>
-                {" "}
-                I enjoy creating things that live on the internet, whether that
-                be websites, applications, or anything in between.
-              </WorkInfo_Desc>
-              <LinksWrapper>
-                <a href="">
-                  <img
-                    className="svg-img"
-                    src="/images/github.svg"
-                    alt="Github Icon"
-                  />
-                </a>
-                <a href="">
-                  <img
-                    className="svg-img"
-                    src="/images/external-link.svg"
-                    alt="External Link Icon"
-                  />
-                </a>
-              </LinksWrapper>
-            </WorkInfo>
-          </WorkImgWrapper>
-        </WorkCard>
-        <WorkCard>
-          <WorkImgWrapper>
-            <WorkImg src="/images/work/work3.jpg" />
-            <WorkInfo>
-              <WorkInfo_Title>Web Dev Design</WorkInfo_Title>
-
-              <WorkInfo_Desc>
-                {" "}
-                I enjoy creating things that live on the internet, whether that
-                be websites, applications, or anything in between.
-              </WorkInfo_Desc>
-              <LinksWrapper>
-                <a href="">
-                  <img
-                    className="svg-img"
-                    src="/images/github.svg"
-                    alt="Github Icon"
-                  />
-                </a>
-                <a href="">
-                  <img
-                    className="svg-img"
-                    src="/images/external-link.svg"
-                    alt="External Link Icon"
-                  />
-                </a>
-              </LinksWrapper>
-            </WorkInfo>
-          </WorkImgWrapper>
-        </WorkCard>
-
-        <WorkCard>
-          <WorkImgWrapper>
-            <WorkImg src="/images/work/work3.jpg" />
-            <WorkInfo>
-              <WorkInfo_Title>Web Dev Design</WorkInfo_Title>
-              {/* */}
-              <WorkInfo_Desc>
-                {" "}
-                I enjoy creating things that live on the internet, whether that
-                be websites, applications, or anything in between.
-              </WorkInfo_Desc>
-              <LinksWrapper>
-                <a href="">
-                  <img
-                    className="svg-img"
-                    src="/images/github.svg"
-                    alt="Github Icon"
-                  />
-                </a>
-                <a href="">
-                  <img
-                    className="svg-img"
-                    src="/images/external-link.svg"
-                    alt="External Link Icon"
-                  />
-                </a>
-              </LinksWrapper>
-            </WorkInfo>
-          </WorkImgWrapper>
-        </WorkCard>
-
-        <WorkCard>
-          <WorkImgWrapper>
-            <WorkImg src="/images/work/work3.jpg" />
-            <WorkInfo>
-              <WorkInfo_Title>Web Dev Design</WorkInfo_Title>
-
-              <WorkInfo_Desc>
-                {" "}
-                I enjoy creating things that live on the internet, whether that
-                be websites, applications, or anything in between.
-              </WorkInfo_Desc>
-              <LinksWrapper>
-                <a href="">
-                  <img
-                    className="svg-img"
-                    src="/images/github.svg"
-                    alt="Github Icon"
-                  />
-                </a>
-                <a href="">
-                  <img
-                    className="svg-img"
-                    src="/images/external-link.svg"
-                    alt="External Link Icon"
-                  />
-                </a>
-              </LinksWrapper>
-            </WorkInfo>
-          </WorkImgWrapper>
-        </WorkCard>
-
-        <WorkCard>
-          <WorkImgWrapper>
-            <WorkImg src="/images/work/work3.jpg" />
-            <WorkInfo>
-              <WorkInfo_Title>Web Dev Design</WorkInfo_Title>
-
-              <WorkInfo_Desc>
-                {" "}
-                I enjoy creating things that live on the internet, whether that
-                be websites, applications, or anything in between.
-              </WorkInfo_Desc>
-              <LinksWrapper>
-                <a href="https://github.com" target="_blank">
-                  <img
-                    className="svg-img"
-                    src="/images/github.svg"
-                    alt="Github Icon"
-                  />
-                </a>
-                <a href="">
-                  <img
-                    className="svg-img"
-                    src="/images/external-link.svg"
-                    alt="External Link Icon"
-                  />
-                </a>
-              </LinksWrapper>
-            </WorkInfo>
-          </WorkImgWrapper>
-        </WorkCard>
-      </WorkContainer>
+                </GitHubLink>
+                <ExternalLink>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="red"
+                    width="30px"
+                    height="30px"
+                  >
+                    <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                    <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                  </svg>
+                </ExternalLink>
+              </LinksContainer>
+            </div>
+          </WorkModal>
+        </WorkGroup>
+      </WorkCard>
     </Container>
   );
 };
